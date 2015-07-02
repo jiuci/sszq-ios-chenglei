12ada7712dbfa50203b8b0f68dbae889c404bc13
panshiyu <panshiyu@idstaff.com>
2015-07-02 11:09:14 +0800

    注册页前两个页面

--- IBY/IBY/UI/regist/BYRegist2VC.m
+++ IBY/IBY/UI/regist/BYRegist2VC.m
@@ -17,6 +17,9 @@
 @property (weak, nonatomic) IBOutlet UILabel* phoneNumLabel;
 @property (weak, nonatomic) IBOutlet UITextField* smsCodeTextField;
 @property (weak, nonatomic) IBOutlet BYAutosizeBgButton* resendBtn;
+@property (weak, nonatomic) IBOutlet UIImageView* bgInput;
+@property (weak, nonatomic) IBOutlet UIImageView* iconInputLeftView;
+@property (weak, nonatomic) IBOutlet UITextField* txtSMSInput;
 
 @property (nonatomic, assign) int countdownTime;
 @property (nonatomic, strong) NSTimer* smsCountTimer;
@@ -27,18 +30,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.title = _titleFromLastPage ? _titleFromLastPage : @"输入验证码";
+    self.title = @"快速注册";
 
     _registService = [[BYRegistService alloc] init];
     _registService.phone = _phone;
 
-    self.phoneNumLabel.text = _registService.phone;
-    
+    self.phoneNumLabel.text = [_registService.phone chineseMobileFormat];
+
+    [self.txtSMSInput setBk_didBeginEditingBlock:^(UITextField* txtField) {
+        self.iconInputLeftView.highlighted = YES;
+        self.bgInput.highlighted = YES;
+    }];
+
+    [self.txtSMSInput setBk_didEndEditingBlock:^(UITextField* txtField) {
+        self.iconInputLeftView.highlighted = NO;
+        self.bgInput.highlighted = NO;
+    }];
 
     self.autoHideKeyboard = YES;
     [self beginTimer];
 }
--(void)viewDidAppear:(BOOL)animated
+- (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
     [self.smsCodeTextField becomeFirstResponder];
@@ -94,19 +106,22 @@
     [self.view endEditing:YES];
 
     self.registService.verifyCode = self.smsCodeTextField.text;
-    NSLog(@"%@",self.registService.verifyCode);
-    [self.registService checkVerifyCode:self.smsCodeTextField.text phone:self.phoneNumLabel.text finish:^(BOOL success, BYError* error) {
-        if(error){
-            alertError(error);
-            [self.smsCodeTextField becomeFirstResponder];
-        }else{
-            BYRegist3VC *setPwdVC = [[BYRegist3VC alloc] init];
-            setPwdVC.registService = self.registService;
-            NSLog(@"%@",self.registService.verifyCode);
-            NSLog(@"%@",setPwdVC.registService.verifyCode);
-            [self.navigationController pushViewController:setPwdVC animated:YES];
-        }
-    }];
+    NSLog(@"%@", self.registService.verifyCode);
+    [self.registService checkVerifyCode:self.smsCodeTextField.text
+                                  phone:_registService.phone
+                                 finish:^(BOOL success, BYError* error) {
+                                     if (error) {
+                                         alertError(error);
+                                         [self.smsCodeTextField becomeFirstResponder];
+                                     }
+                                     else {
+                                         BYRegist3VC* setPwdVC = [[BYRegist3VC alloc] init];
+                                         setPwdVC.registService = self.registService;
+                                         NSLog(@"%@", self.registService.verifyCode);
+                                         NSLog(@"%@", setPwdVC.registService.verifyCode);
+                                         [self.navigationController pushViewController:setPwdVC animated:YES];
+                                     }
+                                 }];
 }
 
 - (IBAction)sendCodeAgainClick:(id)sender
@@ -115,14 +130,16 @@
 }
 
 - (void)fetchSMSCode
-{//这是重发机制，不需要再处理其他类型status
-    [self.registService fetchSMSVerifyCodeWithPhone:_registService.phone finish:^(BYFetchVerifyCodeStatus status, BYError* error) {
-        if(error){
-            alertError(error);
-        }else{
-            [self beginTimer];
-        }
-    }];
+{ //这是重发机制，不需要再处理其他类型status
+    [self.registService fetchSMSVerifyCodeWithPhone:_registService.phone
+                                             finish:^(BYFetchVerifyCodeStatus status, BYError* error) {
+                                                 if (error) {
+                                                     alertError(error);
+                                                 }
+                                                 else {
+                                                     [self beginTimer];
+                                                 }
+                                             }];
 }
 
 @end
