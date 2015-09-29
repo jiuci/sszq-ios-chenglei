3c7548ae4cc1cba9a1b9b25b88d8e8b9032095bc
kangjian <kangjian@idstaff.com>
2015-09-29 17:36:31 +0800

    IMdemo完成

--- IBY/IBY/UI/Instantmessaging/BYIMViewController.m
+++ IBY/IBY/UI/Instantmessaging/BYIMViewController.m
@@ -9,8 +9,9 @@
 #import "BYIMViewController.h"
 #import <EaseMobSDK/EaseMob.h>
 #import <TPKeyboardAvoidingScrollView.h>
+#import "ConvertToCommonEmoticonsHelper.h"
 
-@interface BYIMViewController ()<UITableViewDataSource,UITableViewDelegate,IEMChatProgressDelegate>
+@interface BYIMViewController ()<UITableViewDataSource,UITableViewDelegate,IEMChatProgressDelegate,EMChatManagerChatDelegate,EMChatManagerDelegate,EMCallManagerDelegate>
 @property (nonatomic)UIScrollView * bodyView;
 @property (nonatomic)UITableView * textTable;
 @property (nonatomic)UITextField * inputTextField;
@@ -50,6 +51,9 @@
     
     self.view.autoresizingMask=UIViewAutoresizingFlexibleHeight;
     self.autoHideKeyboard = YES;
+    
+    [[EaseMob sharedInstance].chatManager addDelegate:self delegateQueue:nil];
+//    [[EaseMob sharedInstance].callManager addDelegate:self delegateQueue:nil];
     
     _bodyView = (UIScrollView*)self.view;
     __weak UIScrollView* blockbody = _bodyView;
@@ -108,20 +112,22 @@
 - (void)updateUI
 {
     __weak typeof (self) wself = self;
-    _targetUser = @"iOS0";
+    _targetUser = @"user_16697";
     self.title = _targetUser;
-    [[EaseMob sharedInstance].chatManager asyncLoginWithUsername:@"iOS1" password:@"111111" completion:^(NSDictionary *loginInfo, EMError *error) {
-        if (!error && loginInfo) {
-            NSLog(@"登陆成功");
-            NSLog(@"%@",loginInfo);
-            [wself loadConversation];
-        }
-    } onQueue:nil];
+    if (![EaseMob sharedInstance].chatManager.isLoggedIn) {
+        [[EaseMob sharedInstance].chatManager asyncLoginWithUsername:@"iOS1" password:@"111111" completion:^(NSDictionary *loginInfo, EMError *error) {
+            if (!error && loginInfo) {
+                NSLog(@"登陆成功");
+                NSLog(@"%@",loginInfo);
+                [wself loadConversation];
+            }
+        } onQueue:nil];
+    }
     [self loadConversation];
-    
-    
    
 }
+
+
 
 - (void)viewDidAppear:(BOOL)animated
 {
@@ -147,7 +153,14 @@
 - (void)sendMessage
 {
     [self.view endEditing:YES];
-    
+    NSMutableString * inputtext = [_inputTextField.text mutableCopy];
+    if ([inputtext hasPrefix:@"change"]) {
+        [inputtext replaceCharactersInRange:NSMakeRange(0, 6) withString:@""];
+        _targetUser = inputtext;
+        _inputTextField.text = @"";
+        [self loadConversation];
+        return;
+    }
     NSString * text = [NSString stringWithString:_inputTextField.text];
     EMChatText *txtChat = [[EMChatText alloc] initWithText:text];
     EMTextMessageBody *body = [[EMTextMessageBody alloc] initWithChatObject:txtChat];
@@ -169,6 +182,94 @@
     NSIndexPath * index = [NSIndexPath indexPathForRow:_talkInfo.count - 1 inSection:0];
     [_textTable scrollToRowAtIndexPath:index atScrollPosition:UITableViewScrollPositionNone animated:YES];
 }
+
+-(void)didReceiveMessage:(EMMessage *)message
+{
+    id<IEMMessageBody> msgBody = message.messageBodies.firstObject;
+    switch (msgBody.messageBodyType) {
+        case eMessageBodyType_Text:
+        {
+            // 收到的文字消息
+            NSString *txt = ((EMTextMessageBody *)msgBody).text;
+            
+            NSLog(@"收到的文字是 txt -- %@",txt);
+        }
+            break;
+        case eMessageBodyType_Image:
+        {
+            // 得到一个图片消息body
+            EMImageMessageBody *body = ((EMImageMessageBody *)msgBody);
+            NSLog(@"大图remote路径 -- %@"   ,body.remotePath);
+            NSLog(@"大图local路径 -- %@"    ,body.localPath); // // 需要使用sdk提供的下载方法后才会存在
+            NSLog(@"大图的secret -- %@"    ,body.secretKey);
+            NSLog(@"大图的W -- %f ,大图的H -- %f",body.size.width,body.size.height);
+            NSLog(@"大图的下载状态 -- %lu",(unsigned long)body.attachmentDownloadStatus);
+            
+            
+            // 缩略图sdk会自动下载
+            NSLog(@"小图remote路径 -- %@"   ,body.thumbnailRemotePath);
+            NSLog(@"小图local路径 -- %@"    ,body.thumbnailLocalPath);
+            NSLog(@"小图的secret -- %@"    ,body.thumbnailSecretKey);
+            NSLog(@"小图的W -- %f ,大图的H -- %f",body.thumbnailSize.width,body.thumbnailSize.height);
+            NSLog(@"小图的下载状态 -- %lu",(unsigned long)body.thumbnailDownloadStatus);
+        }
+            break;
+        case eMessageBodyType_Location:
+        {
+            EMLocationMessageBody *body = (EMLocationMessageBody *)msgBody;
+            NSLog(@"纬度-- %f",body.latitude);
+            NSLog(@"经度-- %f",body.longitude);
+            NSLog(@"地址-- %@",body.address);
+        }
+            break;
+        case eMessageBodyType_Voice:
+        {
+            // 音频sdk会自动下载
+            EMVoiceMessageBody *body = (EMVoiceMessageBody *)msgBody;
+            NSLog(@"音频remote路径 -- %@"      ,body.remotePath);
+            NSLog(@"音频local路径 -- %@"       ,body.localPath); // 需要使用sdk提供的下载方法后才会存在（音频会自动调用）
+            NSLog(@"音频的secret -- %@"        ,body.secretKey);
+            NSLog(@"音频文件大小 -- %lld"       ,body.fileLength);
+            NSLog(@"音频文件的下载状态 -- %lu"   ,(unsigned long)body.attachmentDownloadStatus);
+            NSLog(@"音频的时间长度 -- %lu"      ,(long)body.duration);
+        }
+            break;
+        case eMessageBodyType_Video:
+        {
+            EMVideoMessageBody *body = (EMVideoMessageBody *)msgBody;
+            
+            NSLog(@"视频remote路径 -- %@"      ,body.remotePath);
+            NSLog(@"视频local路径 -- %@"       ,body.localPath); // 需要使用sdk提供的下载方法后才会存在
+            NSLog(@"视频的secret -- %@"        ,body.secretKey);
+            NSLog(@"视频文件大小 -- %lld"       ,body.fileLength);
+            NSLog(@"视频文件的下载状态 -- %lu"   ,(unsigned long)body.attachmentDownloadStatus);
+            NSLog(@"视频的时间长度 -- %lu"      ,(long)body.duration);
+            NSLog(@"视频的W -- %f ,视频的H -- %f", body.size.width, body.size.height);
+            
+            // 缩略图sdk会自动下载
+            NSLog(@"缩略图的remote路径 -- %@"     ,body.thumbnailRemotePath);
+            NSLog(@"缩略图的local路径 -- %@"      ,body.thumbnailRemotePath);
+            NSLog(@"缩略图的secret -- %@"        ,body.thumbnailSecretKey);
+            NSLog(@"缩略图的下载状态 -- %lu"      ,(unsigned long)body.thumbnailDownloadStatus);
+        }
+            break;
+        case eMessageBodyType_File:
+        {
+            EMFileMessageBody *body = (EMFileMessageBody *)msgBody;
+            NSLog(@"文件remote路径 -- %@"      ,body.remotePath);
+            NSLog(@"文件local路径 -- %@"       ,body.localPath); // 需要使用sdk提供的下载方法后才会存在
+            NSLog(@"文件的secret -- %@"        ,body.secretKey);
+            NSLog(@"文件文件大小 -- %lld"       ,body.fileLength);
+            NSLog(@"文件文件的下载状态 -- %lu"   ,(unsigned long)body.attachmentDownloadStatus);
+        }
+            break;
+            
+        default:
+            break;
+    }
+    [self loadConversation];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
@@ -187,11 +288,14 @@
     EMMessage * message = _talkInfo[indexPath.row];
     EMTextMessageBody * body = message.messageBodies[0];
     NSString * name = message.from;
-    NSString * text = body.text;
-    cell.textLabel.text = body.text;
+    NSString *didReceiveText = [ConvertToCommonEmoticonsHelper
+                                convertToSystemEmoticons:body.text];
+    cell.textLabel.text = didReceiveText;
     if ([_targetUser isEqualToString:message.from]) {
+        cell.textLabel.textColor = [UIColor blueColor];
         cell.textLabel.textAlignment = NSTextAlignmentLeft;
     }else{
+        cell.textLabel.textColor = [UIColor greenColor];
         cell.textLabel.textAlignment = NSTextAlignmentRight;
     }
     return cell;
